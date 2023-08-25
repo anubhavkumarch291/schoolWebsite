7229ff6b80d0822ab72b96fb047a4b3c3545265f
anubhavkumarch291 <anubhavkumarch291@gmail.com>
2023-08-25 15:40:38 +0530

    Initial commit

---
+++ javascript/index.js
@@ -0,0 +1,83 @@
+const imgData=[
+    {
+        name:'Jordan',
+        src:'https://images.pexels.com/photos/4144095/pexels-photo-4144095.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        name:'Alex Hales',
+        src:'https://images.pexels.com/photos/8363029/pexels-photo-8363029.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        name:'Jonathan',
+        src:'https://images.pexels.com/photos/5212334/pexels-photo-5212334.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        name:'Alex Carry',
+        src:'https://images.pexels.com/photos/5905621/pexels-photo-5905621.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }
+];
+const videoData=[
+    {
+        src:'https://player.vimeo.com/external/554175140.sd.mp4?s=d99498b1bcf1c831873dafce08cf566c2e46d9d2&profile_id=165&oauth2_token_id=57447761'
+    }
+];
+const playImg=[
+    {
+        src:'https://images.pexels.com/photos/8613319/pexels-photo-8613319.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        src:'https://images.pexels.com/photos/8926648/pexels-photo-8926648.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        src:'https://images.pexels.com/photos/8422262/pexels-photo-8422262.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        src:'https://images.pexels.com/photos/8613366/pexels-photo-8613366.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        src:'https://images.pexels.com/photos/8535193/pexels-photo-8535193.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        src:'	https://images.pexels.com/photos/8471799/pexels-photo-8471799.jpeg?auto=compress&cs=tinysrgb&w=400'
+    },
+    {
+        src:'https://images.pexels.com/photos/5427855/pexels-photo-5427855.jpeg?auto=compress&cs=tinysrgb&w=400'
+    },
+    {
+        src:'	https://images.pexels.com/photos/3041176/pexels-photo-3041176.jpeg?auto=compress&cs=tinysrgb&w=400'
+    },
+    {
+        src:'https://images.pexels.com/photos/3425993/pexels-photo-3425993.jpeg?auto=compress&cs=tinysrgb&w=400'
+    },
+    {
+        src:'	https://images.pexels.com/photos/2968077/pexels-photo-2968077.jpeg?auto=compress&cs=tinysrgb&w=400'
+    },
+    {
+        src:'https://images.pexels.com/photos/2202685/pexels-photo-2202685.jpeg?auto=compress&cs=tinysrgb&w=400'
+    },
+    {
+        src:'	https://images.pexels.com/photos/261060/pexels-photo-261060.jpeg?auto=compress&cs=tinysrgb&w=400'
+    },
+    {
+        src:'	https://images.pexels.com/photos/207693/pexels-photo-207693.jpeg?auto=compress&cs=tinysrgb&w=400'
+    },
+    {
+        src:'	https://images.pexels.com/photos/8941640/pexels-photo-8941640.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+
+];
+const pillars=[
+    {
+        name:"Robart Dsoza",
+        src:'https://images.pexels.com/photos/3760514/pexels-photo-3760514.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        name:'Lucy',
+        src:'https://images.pexels.com/photos/8085252/pexels-photo-8085252.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+        name:'Alice Capsey',
+        src:'https://images.pexels.com/photos/864994/pexels-photo-864994.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }
+];
+console.log("hello")
---
+++ ourteam/index.js
@@ -0,0 +1,105 @@
+const homeImg=[
+    {
+         Designation:'Curriculum Engineer',
+        Designation:'',
+        subject:'M.Tech',
+        name:'Ronan',
+        src:'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+         Designation:'Director - Business Head',
+        subject:'MCA',
+        name:'Brayden',
+        src:'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+    {
+         Designation:'Senior Curriculum Engineer',
+        subject:'MBA',
+        name:'Diego',
+        src:'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Curriculum Head',
+        subject:'Ph.d',
+        name:'Antonio',
+        src:'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Director - Curriculum',
+        subject:'M.Com',
+        name:'Marco',
+        src:'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Senior Curriculum Engineer',
+        subject:'M.E',
+        name:'Devin',
+        src:'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Lead Curriculum Engineer',
+        subject:'M.Ped',
+        name:'Ethan',
+        src:'https://images.pexels.com/photos/712513/pexels-photo-712513.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Instructional Associate',
+        subject:'M.Ed',
+        name:'Aaron',
+        src:'https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Instructional Associate',
+        subject:'MBA',
+        name:'Enzo',
+        src:'https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Lead Instructional Associate',
+        subject:'B.Tech',
+        name:'Mathias',
+        src:'https://images.pexels.com/photos/834863/pexels-photo-834863.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Associate Director - HD',
+        subject:'Holistic Development',
+        name:'Clara',
+        src:'https://images.pexels.com/photos/874158/pexels-photo-874158.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Operations Manager - L3',
+        subject:'Software',
+        name:'Nathan',
+        src:'https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Program Manager',
+        subject:'M.Tech',
+        name:'Ines',
+        src:'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Director - Technology',
+        subject:'BCA',
+        name:'Jade',
+        src:'https://images.pexels.com/photos/17983079/pexels-p…sing-on-meta.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Senior Manager - Community',
+        subject:'MFA',
+        name:'Oceana',
+        src:'https://images.pexels.com/photos/17981049/pexels-p…ck-and-white.jpeg?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'Creative Content Producer',
+        subject:'Theather',
+        name:'Luxury',
+        src:'https://images.pexels.com/photos/17986565/pexels-p…ng-his-guitar.png?auto=compress&cs=tinysrgb&w=600'
+    }, 
+    {
+         Designation:'SEO Executive',
+        subject:'MBA',
+        name:'Lorcan',
+        src:'https://images.pexels.com/photos/4467687/pexels-photo-4467687.jpeg?auto=compress&cs=tinysrgb&w=600'
+    },
+];
